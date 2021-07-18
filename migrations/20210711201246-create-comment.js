37f7200720b27ca5f7ba2d0116cfc50cdb505d8b
irmiya001 <irmiyapcklinic@gmail.com>
2021-07-18 11:36:04 +0100

    inialize the project and the model

---
+++ migrations/20210711201246-create-comment.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      postId: {
+        type: Sequelize.INTEGER,
+        onDelete:'CASCADE',
+        references:{
+          model:'Posts',
+          key:'id',
+          as:'postId'
+        },
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
