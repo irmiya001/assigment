0129e2308e730ff703fe7917b1c03020c725c96e
shiniemma1 <shiniemmanuel@outlook.com>
2021-07-18 15:17:34 +0100

    i added the complete routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require('passport');
+const checkerMiddleware = require('../middlewares/checker.middleware');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/post-detail', function(req, res, next) {
+  res.render('postDetail', { title: 'Express' });
+});
+
+router.get(
+  '/profile',
+  function(req, res, next) {
+  res.render('profile', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const userController = require('../controller/user.controller');
+const checkerMiddleware = require('../middlewares/checker.middleware');
+
+const passport = require('passport');
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post(
+  '/register',
+  checkerMiddleware.comparePassword,
+  checkerMiddleware.emailChecker,
+  userController.register
+  );
+
+  router.post('/login', checkerMiddleware.confirmEmail, userController.login)
+  router.post('/make-post', userController.makePost)
+  router.post('/make-comment', userController.makeComment)
+
+  router.delete('/logout', userController.logout)
+
+
+  router.put('/upload_photo', userController.uploadPhoto);
+  router.put('/edit_profile', userController.editProfile);
+
+  router.get('/find_all', userController.findAll);
+  router.get('/get-all-post', userController.getAllPost)
+  router.get('/get-single-post', userController.getSinglePost)
+   
+   router.get(
+     '/find_one',
+      checkerMiddleware.checkLogin,
+      userController.findOne
+      );
 
 module.exports = router;
